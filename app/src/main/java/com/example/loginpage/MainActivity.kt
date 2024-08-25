09c83deef64b854508db2ff332329cec4d21179a
mevraism <ernestiyoguevara@gmail.com>
2024-08-25 23:03:44 +0700

    first commit

---
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.loginpage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.loginpage.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
